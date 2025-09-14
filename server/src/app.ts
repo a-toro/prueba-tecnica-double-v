d19eefa5afe48482e1785b7bde14e427e414948c
William Toro <wtorobenavides@gmail.com>
2025-09-13 22:12:20 -0500

    chore: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { authRoutes } from "./auth/auth-routes";
 
 export const app = express();
 
 // Add middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
