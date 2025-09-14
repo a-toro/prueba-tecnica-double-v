fa69f0344f66880a2d743c29a83251bdd8f72cea
William Toro <wtorobenavides@gmail.com>
2025-09-14 00:28:28 -0500

    feat: add debtsRoutes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { authRoutes } from "./auth/auth-routes";
 import { verifyJWT } from "./middlewares/verify-jwt";
+import { debtsRoutes } from "./debts/debt-routes";
 
 export const app = express();
 
@@ -14,6 +15,8 @@ app.use("/api/auth", authRoutes);
 
 // Rutas protegidas
 app.use(verifyJWT);
+
+app.use("/api/debts", debtsRoutes);
 
 app.get("", (req, res) => {
   res.send("Hello app");
