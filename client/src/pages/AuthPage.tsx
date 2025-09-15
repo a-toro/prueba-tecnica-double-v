87b8b5635758e9051ee09d2b341bc7e9c0280a3f
William Toro <wtorobenavides@gmail.com>
2025-09-14 21:12:01 -0500

    feat: add page for register form

---
+++ client/src/pages/AuthPage.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "../components/RegisterForm";
+
+export default function AuthPage() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <section>
+        <RegisterForm />
+      </section>
+    </div>
+  );
+}
